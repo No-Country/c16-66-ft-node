85edeaca70eb3d227a5eea34e7eb0b5034d74536
Juan Pablo Riglos <juan.riglos@hotmail.com>
2024-02-16 21:12:14 -0300

    Se instalaron las dependencias : Tailwind, reacr-router-dom, material-ui y Zustand. Finalmente se setearon en forma primitiva los Stores (estado global) de Users, Doctors, Admin

--- FrontEnd/fronted-environment/src/App.jsx
+++ FrontEnd/fronted-environment/src/App.jsx
@@ -3,7 +3,7 @@ import { Card, Button } from "@mui/material/";
 function App() {
 	return (
 		<>
-			<Card sx={{ display: "flex", gap: "5%" }}>
+			<Card sx={{ display: "flex", gap: "4%" }}>
 				<h1 className=' text-green-700 font-bold'>No Country Frontend App</h1>
 				<Button sx={{ width: "20%" }} variant='outlined'>
 					Comprobando instalaciones
