58c9902113cc9f78a6ca96dfc030c90e95526a06
Juan Pablo Riglos <juan.riglos@hotmail.com>
2024-02-17 09:03:12 -0300

    Descarga axios para trabajar con Api. Se crearon carpetas padres Components, Pages, Hooks, el archivo service.js el cual queda configurado en blanco y comentado hasta tener Api funcional

---
+++ FrontEnd/fronted-environment/src/Service.js
@@ -0,0 +1,28 @@
+// import axios from "axios";
+
+// export const Api = axios.create({
+//     baseURL :'http://'
+// });
+
+// export const fetchItems = async () => {
+// 	const { data } = await Api.get(`/`);
+// 	return data;
+// };
+
+//
+// export const addItem = async (item) => {
+// 	await Api.post("/", item);
+// };
+
+// export const getOneItem = async (id) => {
+// 	const { data } = await Api.get(`/${id}`);
+// 	return data;
+// };
+
+// export const deleteItem = async (id) => {
+// 	await Api.delete(`/${id}`);
+// };
+
+// export const updateItem = async (item) => {
+// 	await Api.put(`/${item._id}`, item);
+// };
