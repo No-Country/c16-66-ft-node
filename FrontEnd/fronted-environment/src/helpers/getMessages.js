1af5857e87ac4dacdf0de38c4c558ad4113a3703
XEnryux <djaramm10@gmail.com>
2024-03-05 11:09:38 -0500

    componente myAgenda veersion inicial

---
+++ FrontEnd/fronted-environment/src/helpers/getMessages.js
@@ -0,0 +1,20 @@
+export const getMessagesES = () => {
+    return {
+        allDay: 'Todo el día',
+        previous: '<',
+        next: '>',
+        today: 'Hoy',
+        month: 'Mes',
+        week: 'Semana',
+        day: 'Día',
+        agenda: '',
+        date: 'Fecha',
+        time: 'Hora',
+        event: 'Evento',
+        noEventsInRange: '',
+        showMore: total => `+ Ver más (${total})`
+    };
+}
+
+
+
