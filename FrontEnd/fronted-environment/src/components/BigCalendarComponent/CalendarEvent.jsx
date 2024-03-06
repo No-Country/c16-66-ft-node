42cdbb2c0a6c798d2531ae033a3cadc0dfc5220e
XEnryux <djaramm10@gmail.com>
2024-03-06 15:36:44 -0500

    calendarEvent

---
+++ FrontEnd/fronted-environment/src/components/BigCalendarComponent/CalendarEvent.jsx
@@ -0,0 +1,16 @@
+
+export const CalendarEvent = ({event}) => { // props mirar como enviar un turno por props, o si se requiere
+
+   const { title }= event;
+
+      
+    return (
+     <>
+  
+     <strong></strong>
+     <span>{title}</span>
+     </>
+    )
+  }
+  
+  
